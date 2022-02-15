5ed1d32c073af8b1c8dcb694c53fa22071b30679
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2022-02-15 23:44:33 +0200

    15/02/22 23:44

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Navbar.css'
 import Logo from '../../UIKit/Logo/Logo'
+import userLogo from '../../Assets/img/userLogo.png'
 
 function Navbar() {
   return (
     <div className="navbar">
+        <div className="personal">
+          <button className="btnPersonal" >לאזור האישי
+          <img className="userLogo" src={userLogo} alt="" />
+        </button>
+        <p>asd</p>
+        <p>|</p>
+        </div>
         <div className="Navlogo">
             <Logo />
         </div>
