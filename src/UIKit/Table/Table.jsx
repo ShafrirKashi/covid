5c543477c3a4bc0d820a0a07dbd7f9e3e7c4d294
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2022-03-09 22:50:19 +0200

    9.3.22 22:50

--- src/UIKit/Table/Table.jsx
+++ src/UIKit/Table/Table.jsx
@@ -1,61 +1,83 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import { styled } from "@mui/material/styles";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 import './Table.css'
+
+import "./Table.css";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#a7becf",
+    color: theme.palette.common.black,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 
 function createData(code, inCharge, Quan, open, close, status) {
   return { code, inCharge, Quan, open, close, status };
 }
 
 const rows = [
-  
-  createData('16349', "מאיר אמזלג", "24", "12:23","17:22", "פתוחה"),
-  createData('23932', "דנה גרוצקי", "21", "12:23","17:22", "סגורה"),
-  createData('75322', "זאב ג'בוטינסקי", "23", "12:23","17:22", "פתוחה"),
-  createData('45332', "מיכאל רבינוביץ", "88", "12:23","17:22", "סגורה"),
-  createData('73452', "רונית לוי", "22", "12:23","17:22", "פתוחה"),
-
-
+  createData("16349", "מאיר אמזלג", "24", "12:23", "17:22", "פתוחה"),
+  createData("23932", "דנה גרוצקי", "21", "12:23", "17:22", "סגורה"),
+  createData("75322", "זאב ג'בוטינסקי", "23", "12:23", "17:22", "פתוחה"),
+  createData("45332", "מיכאל רבינוביץ", "88", "12:23", "17:22", "סגורה"),
+  createData("73452", "רונית לוי", "22", "12:23", "17:22", "פתוחה"),
 ];
 
 export default function DenseTable() {
   return (
-    <TableContainer component={Paper}>
-      <Table stickyHeader={true} sx={{ Width: "10px" }} size="small" aria-label="a dense table">
-        <TableHead >
-          <TableRow>
-            <TableCell>מק"ט</TableCell>
-            <TableCell align="left">אחראי</TableCell>
-            <TableCell align="left">כמות</TableCell>
-            <TableCell align="left">נפתחה</TableCell>
-            <TableCell align="left">נסגרה</TableCell>
-            <TableCell align="left">סטטוס</TableCell>
-          </TableRow>
+    <Paper sx={{ width: '100%' }}>
+    <TableContainer sx={{ maxHeight: 440 }} >
+      <Table stickyHeader={true} aria-label="sticky table">
+        <TableHead stickyHeader={true} >
+          <StyledTableRow>
+            <StyledTableCell>מק"ט</StyledTableCell>
+            <StyledTableCell align="left">אחראי</StyledTableCell>
+            <StyledTableCell align="left">כמות</StyledTableCell>
+            <StyledTableCell align="left">נפתחה</StyledTableCell>
+            <StyledTableCell align="left">נסגרה</StyledTableCell>
+            <StyledTableCell align="left">סטטוס</StyledTableCell>
+          </StyledTableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <TableRow
+            <StyledTableRow
               key={row.code}
-              sx={{ '&:last-child td, &:last-child th': {  border: 0 } }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
+              <StyledTableCell component="th" scope="row">
                 {row.code}
-              </TableCell>
-              <TableCell align="left">{row.inCharge}</TableCell>
-              <TableCell align="left">{row.Quan}</TableCell>
-              <TableCell align="left">{row.open}</TableCell>
-              <TableCell align="left">{row.close}</TableCell>
-              <TableCell align="left">{row.status}</TableCell>
-            </TableRow>
+              </StyledTableCell>
+              <StyledTableCell align="left">{row.inCharge}</StyledTableCell>
+              <StyledTableCell align="left">{row.Quan}</StyledTableCell>
+              <StyledTableCell align="left">{row.open}</StyledTableCell>
+              <StyledTableCell align="left">{row.close}</StyledTableCell>
+              <StyledTableCell align="left">{row.status}</StyledTableCell>
+            </StyledTableRow>
           ))}
         </TableBody>
       </Table>
     </TableContainer>
+    </Paper>
   );
 }
