bd1cf0a50d21aa90df694cbd1bfc1ac7e033550a
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2022-04-11 13:20:04 +0300

    5

--- src/UIKit/PatientsList/PatientsList.jsx
+++ src/UIKit/PatientsList/PatientsList.jsx
@@ -1,39 +1,74 @@
-import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-import patients from '../../APIcalls/Patients.json'
-import './PatientsList.css'
+import React, { useMemo } from "react";
+import { useTable, useSortBy, useRowSelect,useFilters,useBlockLayout } from "react-table";
+import { DataGrid } from "@mui/x-data-grid";
+import DATA from "../../APIcalls/Patients.json";
+import "./PatientsList.css";
 
-const columns = [
+
+const COLUMNS = [
   {
-    field: 'full_name',
-    headerName: 'שם',
+    Header: "שעת בדיקה",
+    accessor: "time",
+    width: 50,
+    editable: true,
+  },
+  {
+    Header: "שם",
+    accessor: "full_name",
     width: 160,
     editable: true,
   },
   {
-    field: 'test_type',
-    headerName: 'סוג בדיקה',
+    Header: "סוג בדיקה",
+    accessor: "test_type",
     width: 130,
-    editable: true,
-  },
-  {
-    field: 'time',
-    headerName: 'שעה',
-    // type: 'number',
-    width: 80,
     editable: true,
   },
 ];
 
 export default function DataGridDemo() {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => DATA, []);
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable(
+      {
+      columns,
+      data,
+      initialState: {
+        sortBy: [{ id: "time", desc: false }],
+      }
+    },
+    useSortBy,
+    );
+
   return (
-    <div style={{fontFamily:"Rubik", height: "100%", width: '100%' }}>
-      <DataGrid
-        rows={patients}
-        columns={columns}
-        sortingOrder={['asc', 'desc']}
-        disableSelectionOnClick
-        disableColumnMenu      />
+    <div style={{ fontFamily: "Rubik", height: "100%", width: "100%" }}>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
